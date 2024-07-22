f1602139f77e6ed5aae7a63025cd18e8870c16c8
TeriGrizz <167506478+TeriGrizz@users.noreply.github.com>
2024-07-23 00:04:55 +0700

    Add files via upload

---
+++ Tugas Web Perpustakaan/daftarbuku.js
@@ -0,0 +1,9 @@
+function myFunction () {
+    var txt;
+    if(confirm("Maaf buku tidak tersedia")) {
+        txt = "";
+    }   else{
+        txt = "";
+    }
+    document.getElementById("demo").innerHTML = txt;
+}
---
+++ Tugas Web Perpustakaan/footer.js
@@ -0,0 +1,9 @@
+function buttonFooter () {
+    var txt;
+    if(confirm("Maaf halaman tidak tersedia")) {
+        txt = "";
+    }   else{
+        txt = "";
+    }
+    document.getElementById("demo").innerHTML = txt;
+}
